efc1cf866cc057b7d7c0aea3b4f1f2802e6e6d2c
Ben Hauser <ben@hauser.id>
2020-11-29 20:29:04 +0200

    feat: updates to distro script

--- scripts/simulate_fee_claim.py
+++ scripts/simulate_fee_claim.py
@@ -1,4 +1,4 @@
-from brownie import chain, Contract
+from brownie import chain, Contract, ZERO_ADDRESS
 
 # the total accrued fee amount in USD
 # you can get this from the main page of https://www.curve.fi
@@ -14,23 +14,33 @@ def main():
     voting_escrow = Contract("0x5f3b5dfeb7b28cdbd7faba78963ee202a494e2a2")
     admin = distributor.admin()
 
-    minter = "0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7"
-    fee_token.mint(distributor, FEE_AMOUNT * 10**18, {"from": minter})
+    # mint or rug the distributor so it has exactly `FEE_AMOUNT`
+    to_mint = FEE_AMOUNT * 10**10 - fee_token.balanceOf(distributor)
+    if to_mint > 0:
+        minter = "0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7"
+        fee_token.mint(distributor, to_mint, {"from": minter})
+    elif to_mint < 0:
+        fee_token.transfer(ZERO_ADDRESS, abs(to_mint), {'from': distributor})
 
+    # checkpoint the supply and token
     distributor.checkpoint_token({'from': admin})
     distributor.checkpoint_total_supply({'from': admin})
+
+    # sleep and checkpoint again, to ensure we also claim for the epoch where the tokens arrived
     chain.sleep(86400 * 14)
     distributor.checkpoint_token({'from': admin})
     distributor.checkpoint_total_supply({'from': admin})
 
+    initial = fee_token.balanceOf(ADDRESS_TO_CHECK)
     max_epoch = voting_escrow.user_point_epoch(ADDRESS_TO_CHECK)
     epoch = 0
 
-    initial = fee_token.balanceOf(ADDRESS_TO_CHECK)
+    # each call to claim advances the user epoch by a maximum of 50
+    # if an account has increased or extended a lock many times,
+    # it will have to make multiple claim transactions
     while epoch < max_epoch:
         distributor.claim({'from': ADDRESS_TO_CHECK})
         epoch = distributor.user_epoch_of(ADDRESS_TO_CHECK)
 
     claimed = fee_token.balanceOf(ADDRESS_TO_CHECK) - initial
-
     print(f"Amount received: ${claimed/1e18:,.2f}")
